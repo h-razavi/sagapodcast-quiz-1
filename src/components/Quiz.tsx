e9d8b040c6bc2b1f6af33cceb8a91e4d581a57cf
Hossain Razavi <hossain.razavi@gmail.com>
2023-07-18 20:03:39 +0330

    added randomize answers

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Container from "./Container";
 import QuizOption from "./QuizOption";
 import quizData, { AnswerType } from "../data/data";
+import Button from "./Button";
 
 type Props = {
   onNext: () => void;
@@ -10,20 +11,25 @@ type Props = {
 function Quiz({ onNext }: Props) {
   const [currentQuestionIndex, setCurrentQuestionInex] = useState(0);
   const [currentUserScore, setCurrentUserScore] = useState(0);
+  const [quizIsCompleted , setQuizIsCompleted] = useState(false)
+
+  const currentQuestion = quizData[currentQuestionIndex];
+
 
   localStorage.setItem("userScore", currentUserScore.toLocaleString());
 
   function handleSelectOption(answer: AnswerType) {
-    setCurrentUserScore((prevScore) => prevScore + answer.point);
-    setCurrentQuestionInex((prevQuestionIndex) => prevQuestionIndex + 1);
-    if (currentQuestion.isLastQuestion) {
-      onNext();
-      setCurrentQuestionInex((prevQuestionIndex) => prevQuestionIndex + 1);
+    if(!currentQuestion.isLastQuestion){
+     setCurrentUserScore((prevScore) => prevScore + answer.point);
+     setCurrentQuestionInex((prevQuestionIndex) => prevQuestionIndex + 1);
+    } else if (currentQuestion.isLastQuestion) {
+      setCurrentUserScore((prevScore) => prevScore + answer.point);
       localStorage.setItem("userScore", currentUserScore.toLocaleString());
+      setQuizIsCompleted(true)
     }
   }
 
-  const currentQuestion = quizData[currentQuestionIndex];
+
 
   return (
     <section className="text-center">
@@ -32,16 +38,16 @@ function Quiz({ onNext }: Props) {
           {currentQuestion.question}
         </h2>
         <ul>
-          {currentQuestion.answers.map((answer) => (
+          {currentQuestion.answers.sort(()=>Math.random()-0.5).map((answer) => (
             <li key={answer.text}>
               <QuizOption
-                onSelect={() => handleSelectOption(answer)}
+                onSelect={!quizIsCompleted?() => handleSelectOption(answer):()=>{}}
                 text={answer.text}
               />
             </li>
           ))}
         </ul>
-
+        {quizIsCompleted&&<Button onNext={onNext}>ادامه</Button>}
         <p>امتیاز شما : {currentUserScore}</p>
       </Container>
     </section>
